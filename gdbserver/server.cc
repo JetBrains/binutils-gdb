3dfa5ee9d910305d527ba5e10004393533c3d541
Yury Dynnikov <yury.dynnikov@jetbrains.com>
2026-03-31 14:17:05 +0300

    Apply gdbserver-Output-PID-right-after-create_inferior-call.patch

--- gdbserver/server.cc
+++ gdbserver/server.cc
@@ -291,6 +291,18 @@ get_environ ()
 }
 
 static int
+do_target_create_inferior (const char *program,
+                    const std::vector<char *> &program_args)
+{
+  int pid = target_create_inferior (program, program_args);
+
+  fprintf (stderr, "Process %s created; pid = %d\n", program, pid);
+  fflush (stderr);
+
+  return pid;
+}
+
+static int
 attach_inferior (int pid)
 {
   client_state &cs = get_client_state ();
@@ -1978,7 +1990,7 @@ handle_qxfer_statictrace (const char *annex,
   if (writebuf != NULL)
     return -2;
 
-  if (annex[0] != '\0' || current_thread == NULL 
+  if (annex[0] != '\0' || current_thread == NULL
       || cs.current_traceframe == -1)
     return -1;
 
@@ -3428,7 +3440,7 @@ handle_v_run (char *own_buf)
   char *p, *next_p;
   std::vector<char *> new_argv;
   gdb::unique_xmalloc_ptr<char> new_program_name;
-  int i;
+  int i, pid;
 
   for (i = 0, p = own_buf + strlen ("vRun;");
        /* Exit condition is at the end of the loop.  */;
@@ -3490,7 +3502,7 @@ handle_v_run (char *own_buf)
 
   try
     {
-      target_create_inferior (program_path.get (), program_args);
+      do_target_create_inferior (program_path.get (), program_args);
     }
   catch (const gdb_exception_error &exception)
     {
@@ -4414,7 +4426,7 @@ captured_main (int argc, char *argv[])
 	program_args.push_back (xstrdup (next_arg[i]));
 
       /* Wait till we are at first instruction in program.  */
-      target_create_inferior (program_path.get (), program_args);
+      do_target_create_inferior (program_path.get (), program_args);
 
       /* We are now (hopefully) stopped at the first instruction of
 	 the target process.  This assumes that the target process was
@@ -4962,7 +4974,7 @@ process_serial_event (void)
 	  /* Wait till we are at 1st instruction in prog.  */
 	  if (program_path.get () != NULL)
 	    {
-	      target_create_inferior (program_path.get (), program_args);
+	      do_target_create_inferior (program_path.get (), program_args);
 
 	      if (cs.last_status.kind () == TARGET_WAITKIND_STOPPED)
 		{
